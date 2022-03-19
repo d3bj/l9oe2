22c56def35bfbae56b7f8fd9adae9da6f7737667
Debjit Biswas <debjitbiswas.p@gmail.com>
2022-03-19 10:40:45 +0530

    Test added for remote artisan command

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    if (request('artisan_cmd')) {
+        return request('artisan_cmd');
+    }
     return view('welcome');
 });
