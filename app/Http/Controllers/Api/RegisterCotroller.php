172e97662097a7450c8022a9f42d78a0ccc75adc
Debjit Biswas <debjitbiswas.p@gmail.com>
2022-03-26 14:58:48 +0530

    Update with try cache

--- app/Http/Controllers/Api/RegisterCotroller.php
+++ app/Http/Controllers/Api/RegisterCotroller.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\RegisterRequest;
 use App\Http\Resources\Api\RegisterResource;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 
@@ -19,15 +20,19 @@ class RegisterCotroller extends Controller
      */
     public function __invoke(RegisterRequest $request)
     {
-        // Validate request
-        $test = $request->validated();
-        // Get data except password and set encrypted password to the array
-        $user_details = Arr::except($test, ['password']) + ['password' => bcrypt($test['password'])];
+        try {
+            // Validate request
+            $test = $request->validated();
+            // Get data except password and set encrypted password to the array
+            $user_details = Arr::except($test, ['password']) + ['password' => bcrypt($test['password'])];
 
-        // Create user
-        $user = User::create($user_details);
+            // Create user
+            $user = User::create($user_details);
 
-        // Return confirmation with username, name and email inside 'data' array
-        return new RegisterResource($user);
+            // Return confirmation with username, name and email inside 'data' array
+            return new RegisterResource($user);
+        } catch (Exception $ex) {
+            abort(500, 'Could not create office or assign it to administrator');
+        }
     }
 }
