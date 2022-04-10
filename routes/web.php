b1d8484af972cc5571d7f67feb6e150ee33bbafd
Debjit Biswas <debjitbiswas.p@gmail.com>
2022-04-11 00:05:46 +0530

    Updated Homepage with json response

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(
+        [
+            'status' => 'error',
+            'message' => '404 endpoint not found. This is the base URL for the API and does not return anything itself. Please check the API reference at https://alpana.org/openexam to find a valid API endpoint.',
+            'payload' => null,
+        ],
+        404
+    );
 });
 Route::get('/test', function () {
     return 'test';
